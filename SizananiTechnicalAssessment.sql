629d97b14ebba31cafd0b156108189498e02b3b1
Gianluca Palmarozza <80106783+GianlucaPal@users.noreply.github.com>
2023-11-03 22:56:06 +0200

    SQL changes

--- SizananiTechnicalAssessment.sql
+++ SizananiTechnicalAssessment.sql
@@ -1,5 +1,6 @@
-
-/****** Object:  Table [dbo].[Users]    Script Date: 2023/11/03 00:47:01 ******/
+USE [SizananiTecnicalAssessment]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 2023/11/03 22:53:55 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -18,7 +19,7 @@ CREATE TABLE [dbo].[Users](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Vehicles]    Script Date: 2023/11/03 00:47:01 ******/
+/****** Object:  Table [dbo].[Vehicles]    Script Date: 2023/11/03 22:53:55 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -36,7 +37,7 @@ CREATE TABLE [dbo].[Vehicles](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[VehicleTypes]    Script Date: 2023/11/03 00:47:02 ******/
+/****** Object:  Table [dbo].[VehicleTypes]    Script Date: 2023/11/03 22:53:55 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -50,22 +51,6 @@ CREATE TABLE [dbo].[VehicleTypes](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-GO
-SET IDENTITY_INSERT [dbo].[VehicleTypes] ON 
-GO
-INSERT [dbo].[VehicleTypes] ([TypeID], [Type]) VALUES (1, N'Sedan')
-GO
-INSERT [dbo].[VehicleTypes] ([TypeID], [Type]) VALUES (2, N'Bakkie')
-GO
-INSERT [dbo].[VehicleTypes] ([TypeID], [Type]) VALUES (3, N'Truck')
-GO
-INSERT [dbo].[VehicleTypes] ([TypeID], [Type]) VALUES (4, N'SUV')
-GO
-INSERT [dbo].[VehicleTypes] ([TypeID], [Type]) VALUES (5, N'Van')
-GO
-SET IDENTITY_INSERT [dbo].[VehicleTypes] OFF
-GO
 ALTER TABLE [dbo].[Users] ADD  CONSTRAINT [DF_Users_UserID]  DEFAULT (newid()) FOR [UserID]
 GO
 ALTER TABLE [dbo].[Vehicles]  WITH CHECK ADD  CONSTRAINT [FK_Vehicles_Users] FOREIGN KEY([UserID])
@@ -78,7 +63,7 @@ REFERENCES [dbo].[VehicleTypes] ([TypeID])
 GO
 ALTER TABLE [dbo].[Vehicles] CHECK CONSTRAINT [FK_Vehicles_VehicleTypes]
 GO
-/****** Object:  StoredProcedure [dbo].[spGetUserLogins]    Script Date: 2023/11/03 00:47:02 ******/
+/****** Object:  StoredProcedure [dbo].[spGetUserLogins]    Script Date: 2023/11/03 22:53:55 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -103,11 +88,32 @@ SELECT U.[UserID]
 END
 
 GO
-/****** Object:  StoredProcedure [dbo].[spType_List]    Script Date: 2023/11/03 00:47:02 ******/
+/****** Object:  StoredProcedure [dbo].[spSummary_List]    Script Date: 2023/11/03 22:53:55 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
+CREATE   PROCEDURE [dbo].[spSummary_List]
+(@UserID AS UNIQUEIDENTIFIER = NULL) --Accepts null to display all users as well 
+AS
+BEGIN
+SELECT 
+CONCAT(U.[FirstName], ' ', U.[LastName]) AS [FullName]
+,(SELECT COUNT([VehicleID]) FROM [Vehicles] AS V1 WHERE U.[UserID] = V1.[UserID]) AS [NoOfVehicles]  
+,(SELECT SUM([Weight]) FROM [Vehicles] AS V2 WHERE U.[UserID] = V2.[UserID]) AS [TotalTons]  
+FROM [Users] AS U
+WHERE (@UserID IS NULL OR @UserID = U.[UserID])
+END
+
+
+GO
+/****** Object:  StoredProcedure [dbo].[spType_List]    Script Date: 2023/11/03 22:53:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
 CREATE   PROCEDURE [dbo].[spType_List]
 AS
 BEGIN
@@ -115,8 +121,9 @@ SELECT[TypeID]
       ,[Type]
   FROM [dbo].[VehicleTypes]
 END 
+
 GO
-/****** Object:  StoredProcedure [dbo].[spUser_Insert]    Script Date: 2023/11/03 00:47:02 ******/
+/****** Object:  StoredProcedure [dbo].[spUser_Insert]    Script Date: 2023/11/03 22:53:55 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -154,8 +161,11 @@ INSERT INTO [dbo].[Users]
 END
 
 GO
-/****** Object:  StoredProcedure [dbo].[spUser_List]    Script Date: 2023/11/03 00:47:02 ******/
-
+/****** Object:  StoredProcedure [dbo].[spUser_List]    Script Date: 2023/11/03 22:53:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE   PROCEDURE [dbo].[spUser_List]
 (@UserID AS UNIQUEIDENTIFIER)
 AS 
@@ -173,7 +183,7 @@ SELECT U.[UserID]
 END
 
 GO
-/****** Object:  StoredProcedure [dbo].[spUser_Update]    Script Date: 2023/11/03 00:47:02 ******/
+/****** Object:  StoredProcedure [dbo].[spUser_Update]    Script Date: 2023/11/03 22:53:55 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -200,11 +210,12 @@ WHERE [UserID] = @UserID
 END
 
 GO
-/****** Object:  StoredProcedure [dbo].[spVehicles_Delete]    Script Date: 2023/11/03 00:47:02 ******/
+/****** Object:  StoredProcedure [dbo].[spVehicles_Delete]    Script Date: 2023/11/03 22:53:55 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 CREATE   PROCEDURE [dbo].[spVehicles_Delete]
 (@VehicleID AS INT)
 AS 
@@ -213,7 +224,7 @@ DELETE FROM  [dbo].[Vehicles]
 WHERE (@VehicleID = [VehicleID])
 END
 GO
-/****** Object:  StoredProcedure [dbo].[spVehicles_Insert]    Script Date: 2023/11/03 00:47:02 ******/
+/****** Object:  StoredProcedure [dbo].[spVehicles_Insert]    Script Date: 2023/11/03 22:53:55 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -240,16 +251,18 @@ INSERT INTO [dbo].[Vehicles]
 			,@Weight
 			,@TypeID
 			,@UserID)
+	SET @VehicleID = SCOPE_IDENTITY()
 END
 GO
-/****** Object:  StoredProcedure [dbo].[spVehicles_ListByUser]    Script Date: 2023/11/03 00:47:02 ******/
+/****** Object:  StoredProcedure [dbo].[spVehicles_ListByUser]    Script Date: 2023/11/03 22:53:55 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE   PROCEDURE [dbo].[spVehicles_ListByUser]
-(@UserID AS UNIQUEIDENTIFIER = null)
+(@UserID AS UNIQUEIDENTIFIER = NULL
+,@VehicleID AS INT = NULL)
 AS
 BEGIN
 SELECT V.[VehicleID]
@@ -263,11 +276,12 @@ SELECT V.[VehicleID]
   FROM [dbo].[Vehicles] AS V
 LEFT OUTER JOIN [VehicleTypes] AS VT ON VT.[TypeID] = V.[TypeID]
 LEFT OUTER JOIN [Users] AS U ON U.[UserID] = V.[UserID]
-WHERE(@UserID = V.[UserID])
+WHERE(@UserID IS NULL OR @UserID = V.[UserID])
+AND ((@VehicleID IS Null OR @VehicleID = 0) OR @VehicleID = [VehicleID])
 END
 
 GO
-/****** Object:  StoredProcedure [dbo].[spVehicles_Update]    Script Date: 2023/11/03 00:47:02 ******/
+/****** Object:  StoredProcedure [dbo].[spVehicles_Update]    Script Date: 2023/11/03 22:53:55 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -290,4 +304,20 @@ SET [RegistrationNumber] = @RegistrationNumber
 ,[UserID] = @UserID
 WHERE (@VehicleID = [VehicleID])
 END
+GO
+
+GO
+SET IDENTITY_INSERT [dbo].[VehicleTypes] ON 
+GO
+INSERT [dbo].[VehicleTypes] ([TypeID], [Type]) VALUES (1, N'Sedan')
+GO
+INSERT [dbo].[VehicleTypes] ([TypeID], [Type]) VALUES (2, N'Bakkie')
+GO
+INSERT [dbo].[VehicleTypes] ([TypeID], [Type]) VALUES (3, N'Truck')
+GO
+INSERT [dbo].[VehicleTypes] ([TypeID], [Type]) VALUES (4, N'SUV')
+GO
+INSERT [dbo].[VehicleTypes] ([TypeID], [Type]) VALUES (5, N'Van')
+GO
+SET IDENTITY_INSERT [dbo].[VehicleTypes] OFF
 GO
